2de02fc5a5c77c7ae45550368765071326668479
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-01-02 13:35:18 +0500

    reading product routes

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -5,13 +5,30 @@ const { errorHandler } = require("../helpers/dbErrorHandler");
 const fs = require("fs");
 const { result } = require("lodash");
 
+exports.productByID = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        err: "product not found",
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
-        err: "Image could not br uploaded",
+        err: "Image could not be uploaded",
       });
     }
     let product = new Product(fields);
@@ -20,6 +37,19 @@ exports.create = (req, res) => {
       if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: "Image should be less than 1mb in size",
+        });
+      }
+      const { name, description, price, category, quantity, shipping } = fields;
+      if (
+        !name ||
+        !description ||
+        !price ||
+        !category ||
+        !quantity ||
+        !shipping
+      ) {
+        return res.status(400).json({
+          error: "all fields are required!",
         });
       }
       product.photo.data = fs.readFileSync(files.photo.filepath); // change path to filepath
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 
-const { create } = require("../controllers/product");
+const { create, productByID, read } = require("../controllers/product");
 const { userByID } = require("../controllers/user");
 
+router.get("/product/:productID", read);
 router.post("/product/create/:userID", requireSignin, isAdmin, isAuth, create);
 
 router.param("userID", userByID);
+router.param("productID", productByID);
 
 module.exports = router;
